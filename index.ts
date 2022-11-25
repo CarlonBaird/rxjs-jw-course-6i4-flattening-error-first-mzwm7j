43ec76ab489f39b58833763f8af61577e18e442b
CarlonBaird <carlyestrella@live.com>
2022-11-25 10:08:35 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -1,15 +1,21 @@
-import { fromEvent } from "rxjs";
-import { ajax } from "rxjs/ajax";
-import { concatMap, map } from "rxjs/operators";
+import { EMPTY, fromEvent } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
+import { catchError, concatMap, map } from 'rxjs/operators';
 
-const endpointInput: HTMLInputElement = document.querySelector('input#endpoint');
+const endpointInput: HTMLInputElement =
+  document.querySelector('input#endpoint');
 const fetchButton = document.querySelector('button#fetch');
 
-fromEvent(fetchButton, 'click').pipe(
-  map(() => endpointInput.value),
-  concatMap(value =>
-    ajax(`https://random-data-api.com/api/${value}/random_${value}`)
+//Search: not-existing-endpoint
+fromEvent(fetchButton, 'click')
+  .pipe(
+    map(() => endpointInput.value),
+    concatMap((value) =>
+      ajax(`https://random-data-api.com/api/${value}/random_${value}`)
+    ),
+    catchError(() => EMPTY) //this emits a complete notification so that the subscription will not end if an error occurs.
   )
-).subscribe(
-  value => console.log(value)
-);
+  .subscribe({
+    next: (value) => console.log(value),
+    error: (err) => console.log('Error:', err),
+  });
